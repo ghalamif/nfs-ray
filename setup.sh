69147baed96a169f550fce1123b9ca3249cb956e
ghalamif <ghalamif@gmail.com>
2024-08-13 19:28:26 +0200

    update setup.bash

--- setup.sh
+++ setup.sh
@@ -1,29 +1,45 @@
 #!/bin/bash
 
-#gpu_nodes=("butter" "coffee")
 namespace="kube-ray"
 service_account="kuberay-service-account"
 ray_cluster_value_path="kuube-ray/ray-cluster-values.yaml"
 
-node_exists() {
-    kubectl get node "$1" &> /dev/null
-    return $?
-}
-
-nvidia_plugin_exists() {
-    kubectl get daemonset nvidia-device-plugin-daemonset -n kube-system &> /dev/null
-    return $?
-}
-
 install_nvidia_device_plugin() {
-    if nvidia_plugin_exists; then
-        echo "NVIDIA device plugin already installed. Skipping."
-    else
-        echo "Installing NVIDIA device plugin."
-        kubectl apply -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v0.14.0/nvidia-device-plugin.yml
-    fi
-}
 
+    # Check if the 'gpu-operator' namespace exists
+    if ! kubectl get ns gpu-operator >/dev/null 2>&1; then
+        echo "Creating 'gpu-operator' namespace."
+        kubectl create ns gpu-operator
+    else
+        echo "'gpu-operator' namespace already exists."
+    fi
+
+    # Label the 'gpu-operator' namespace with the necessary security context
+    echo "Labeling 'gpu-operator' namespace for privileged pod security."
+    kubectl label --overwrite ns gpu-operator pod-security.kubernetes.io/enforce=privileged
+
+    # Add NVIDIA Helm repository if it doesn't exist
+    if ! helm repo list | grep -q "nvidia"; then
+        echo "Adding NVIDIA Helm repository."
+        helm repo add nvidia https://helm.ngc.nvidia.com/nvidia
+    else
+        echo "NVIDIA Helm repository already added."
+    fi
+
+    # Update the Helm repository to ensure we have the latest charts
+    echo "Updating Helm repository."
+    helm repo update
+
+    # Install the NVIDIA GPU Operator if it's not already installed
+    if ! helm ls -n gpu-operator | grep -q "gpu-operator"; then
+        echo "Installing NVIDIA GPU Operator."
+        helm install --wait --generate-name nvidia/gpu-operator --set operator.defaultRuntime="containerd" -n gpu-operator
+    else
+        echo "NVIDIA GPU Operator already installed."
+    fi
+
+    echo "NVIDIA device plugin installation complete."
+}
 
 create_namespace() {
     # Check if the namespace already exists and create it if it doesn't
@@ -73,10 +89,39 @@ upgrade_ray_cluster() {
     helm upgrade ray-cluster kuberay/ray-cluster -f $ray_cluster_value_path --namespace $namespace
 }
 
-################################################################################
-# Call the function with the global gpu_nodes array
-################################################################################
-label_gpu_nodes "${gpu_nodes[@]}"
+delete_kube_ray_operator_and_cluster() {
+    # Delete the Ray cluster if it exists
+    if helm list -n $namespace | grep -q ray-cluster; then
+        echo "Deleting Ray cluster."
+        helm uninstall ray-cluster --namespace $namespace
+    else
+        echo "Ray cluster not found. Skipping."
+    fi
+
+    # Delete the Ray operator if it exists
+    if helm list -n $namespace | grep -q kuberay; then
+        echo "Deleting KubeRay operator."
+        helm uninstall kuberay --namespace $namespace
+    else
+        echo "KubeRay operator not found. Skipping."
+    fi
+
+    # Delete the service account if it exists
+    if kubectl get serviceaccount $service_account -n $namespace &> /dev/null; then
+        echo "Deleting service account $service_account."
+        kubectl delete serviceaccount $service_account -n $namespace
+    else
+        echo "Service account $service_account not found. Skipping."
+    fi
+
+    # Delete the namespace if it exists
+    if kubectl get namespace $namespace &> /dev/null; then
+        echo "Deleting namespace $namespace."
+        kubectl delete namespace $namespace
+    else
+        echo "Namespace $namespace not found. Skipping."
+    fi
+}
 
 ################################################################################
 # Call the function to install the NVIDIA device plugin for k8s
